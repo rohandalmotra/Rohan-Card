4c287413b8030d492af2546809191441368e0613
Rohan Dalmotra <rohandalmotra1@gmail.com>
2022-07-15 13:46:35 +0530

    Initial Commit

---
+++ Rohan Card/Rohan_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rohan_CardApp.swift
+//  Rohan Card
+//
+//  Created by Rohan Dalmotra on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Rohan_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
